16d4efb699b3d5eb0342f317dc3b4083afbc8826
CJ Black <cj@tradedefender.com>
2018-08-06 23:27:18 -0500

    finally fucking works

---
+++ env.py
@@ -0,0 +1,22 @@
+
+BAD_DOMAINS = ['intercom-mail.com', 'mailchimp.com', 'tradedefender.com',
+               'filabs.io', 'mx.lakeforest.edu', 'oscillas.com', 'google.com',
+               'adobe.com', 'docker.com', 'lessannoyingcrm.com', 'chase.com',
+               'email.skype.com', 'bitfinex.com', 'google.com', 'paypal.com',
+               'xignite.com', 'uberconference.com', 'intercom.io', 'angel.co',
+               'craigslist.org', 'upwork.com', 'amazon.com', 'dialpad.com',
+               'google.com', 'microsoft.com', 'dowjones.com', 'bitly.com',
+               'digitalocean.com', 'linkedin.com', 'twitter.com', 'aclu.org',
+               'intuit.com', 'hootsuite.com', 'luc.edu', 'freshbooks.com',
+               'tweepi.com', 'zendesk.com', 'ovh.net', 'uber.com', 'hubspot.com',
+               'nytimes.com', 'slack.com', 'eventbrite.com', 'envato.com',
+               '.edu', 'trello.com', 'ifttt.com', 'gofundme.com', 'bankofamerica.com',
+               'sedo.com', 'anewstip.com', 'nbcuni.com', 'clubhouse.io', 'foursquare.com',
+               'upcounsel.io', '.gov', 'mailgun.net', 'cmtassociation.org']
+
+BAD_PREFIXES = ['info', 'information', 'support', 'sales', 'help',
+               'noreply', 'donotreply', 'team', 'hello', 'hq', 'robot',
+               'newsletter', 'admin', 'hi', 'connect', 'no-reply', 'replies',
+               'reply', 'daemon', 'welcome', 'news']
+
+BAD_CHARS = ['\r', '\n', '\t', '\'', '\"']
---
+++ main.py
@@ -0,0 +1,120 @@
+import sys
+import mailbox
+import env
+
+RAW_EMAILS = []
+FINISHED_EMAILS = []
+
+MSG_COUNT = 0
+
+BAD_CHARS = env.BAD_CHARS
+BAD_PREFIXES = env.BAD_PREFIXES
+BAD_DOMAINS = env.BAD_DOMAINS
+
+def readMbox(mboxPath):
+    global RAW_EMAILS
+    mbox = mailbox.mbox(mboxPath)
+
+    for msg in mbox:
+        stripEmails(msg)
+
+    RAW_EMAILS = list(set(RAW_EMAILS))
+
+def stripEmails(msg):
+    global RAW_EMAILS, MSG_COUNT
+    MSG_COUNT += 1
+    emails = []
+
+    keys = list(msg.keys())
+
+    if 'To' in keys:
+        toAddrs = removeChars(msg['To']).split(',')
+        emails += toAddrs
+
+    if 'From' in keys:
+        fromAddrs = removeChars(msg['From']).split(',')
+        emails += fromAddrs
+
+    if 'Cc' in keys:
+        ccAddrs = removeChars(msg['Cc']).split(',')
+        emails += ccAddrs
+
+    if 'Bcc' in keys:
+        bccAddrs = removeChars(msg['Bcc']).split(',')
+        emails += bccAddrs
+
+    emails = list(set(emails))
+    RAW_EMAILS += emails
+
+    if MSG_COUNT % 1000 == 0:
+        RAW_EMAILS = list(set(RAW_EMAILS))
+
+def removeBadElements():
+    count = 0
+
+    for email in RAW_EMAILS:
+        name = ''
+        addr = ''
+        goodResult = True
+
+        if '<' in email:
+            name = removeErrantSpaces(email[:email.index('<')])
+            addr = removeErrantSpaces(email[email.index('<') + 1:email.index('>')])
+
+        else:
+            addr = removeErrantSpaces(email)
+
+        if '@' in addr:
+            domain = addr[addr.index('@')+1:].lower()
+            prefix = addr[:addr.index('@')].lower()
+
+            for x in BAD_PREFIXES:
+                if x in prefix:
+                    goodResult = False
+
+            for x in BAD_DOMAINS:
+                if x in domain:
+                    goodResult = False
+
+        else:
+            goodResult = False
+
+        if goodResult:
+            count += 1
+            print(addr, count)
+            #pass
+
+def removeChars(x):
+    for char in BAD_CHARS:
+        x = x.replace(char, '')
+
+    return(x)
+
+def removeErrantSpaces(x):
+    if len(x) > 1:
+        while x[0] == ' ':
+            if len(x) > 1:
+                x = x[1:]
+
+            else:
+                return('')
+
+        while x[-1] == ' ':
+            if len(x) > 1:
+                x = x[:-1]
+
+            else:
+                return(x)
+
+        return(x)
+
+    else:
+        return('')
+
+def execute(mboxPath):
+    readMbox(mboxPath)
+
+    removeBadElements()
+
+if __name__ == '__main__':
+    execute(sys.argv[1])
